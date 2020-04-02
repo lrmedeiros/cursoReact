b107613123bd486ec378192a1b0f3c391728f1f2
Lucas Rinaldi Medeiros Silva <lrmedeiros10@gmail.com>
2020-04-02 03:52:35 -0300

    Curso de reactjs

---
+++ src/pages/main/index.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import api from '../../services/api';
+import { Link } from 'react-router-dom';
+
+import './styles.css';
+
+export default class Main extends Component {
+    state = {
+        products: [],
+        productInfo: {},
+        page: 1,
+    };
+
+    componentDidMount() {
+        this.loadProducts();
+    }
+
+    nextPage = () => {
+        const { page, productInfo } = this.state;
+
+        if(page === productInfo.pages) return;
+
+        const pageNumber = page + 1;
+
+        this.loadProducts(pageNumber);
+    }
+    
+    prevPage = () => {
+        const { page } = this.state;
+        if(page === 1) return;
+
+        const pageNumber = page - 1;
+
+        this.loadProducts(pageNumber);
+    }
+
+    loadProducts = async (page = 1) => {
+        const response = await api.get(`/products?page=${page}`);
+
+        const { docs, ...productInfo } = response.data;
+
+        this.setState({ products: docs, productInfo, page });
+    };
+
+    render() {
+        const { page, productInfo } = this.state;
+
+        return (
+            <div className="product-list">
+                {this.state.products.map(products => (
+                    <article key={products._id}>
+                        <strong>{products.title}</strong>
+                        <p>{products.description}</p>
+                        <Link to={`products/${products._id}`}>Acessar</Link>
+                    </article>
+                ))}
+
+                <div className="actions">
+                    <button disabled={page===1} onClick={this.prevPage}>Anterior</button>
+                    <button disabled={page===productInfo.pages}onClick={this.nextPage}>Próximo</button>
+                </div>
+            </div>
+        )
+    }
+}
